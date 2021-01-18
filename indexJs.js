ee1967527f42538729b78f485e19796ec06e6737
Diego Jiménez Urgell <dieji@hotmail.es>
2021-01-18 12:40:07 -0600

    First commit

---
+++ indexJs.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+function changeImg(id){
+    if (screen.width >= 580) {
+        loadImage(id, "images/quienesNinosTitle.png");
+        document.getElementById(id).src = "images/quienesNinosTitle.png";
+    } else {
+        document.getElementById(id).src = "images/quienesNinosTitleMobile.png";
+    }
+}
+
+function loadImage(elem, path){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            return true;
+        }
+        else return false;
+    }
+    xhttp.open("GET", path, false);
+    xhttp.send();
+    console.log(xhttp.responseURL);
+    return xhttp.responseURL;
+}
+
+changeImg("quienesNinosImage");
+window.addEventListener("resize", changeImg("quienesNinosImage"));
+*/
