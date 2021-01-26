a69009d1983976cbccf4f44b1f1c0f52d25cfcad
Diego Jiménez Urgell <diegourgell@outlook.com>
2021-01-26 02:08:03 -0600

    vp1-02: Added mobile navbar and appearance. Missing navbar closing. Missing community adjustments and campaign

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,10 +1,59 @@
 
+window.addEventListener("load", loadLinks);
+window.addEventListener("load", markSelected);
+
+function loadLinks(){
+    let frame = document.getElementById("navBar").contentDocument;
+    let smallNav = frame.getElementById("small-navbar-button");
+    let donate = frame.getElementById("donate-btn");
+    smallNav.onclick = openNav;
+    donate.onclick = () => window.open("https://l.instagram.com/?u=https%3A%2F%2Fwww.paypal.com%2Fdonate%3Fhosted_button_id%3DQT4ZRWJ5Z28T6&e=ATOFlPDQL-3Kna_8rBjvYfZnbBwRyPK2cw3dB91T81xS-wJ5icvBgxQ2pJSdt0lh9TNkdmUZ9_WM9j0oRqMdoLStvWhJSejqUKO-kQ&s=1")
+    console.log(smallNav.onclick);
+    let links = frame.getElementsByClassName("menuLink");
+    for(let i = 0; i < links.length;i++) {
+        console.log(links[i].id);
+        links[i].onclick = function(){
+            window.sessionStorage["select"] = this.id;
+            window.location = this.dataset.href;
+        }
+    }
+};
+
+function markSelected(){
+    let frame = document.getElementById("navBar").contentDocument;
+    let cont = frame.getElementsByClassName("selected");
+    if (cont.length > 0) cont[0].classList.remove("selected");
+    frame.getElementById(window.sessionStorage.getItem("select")).classList.add("selected");
+};
+
+function openNav(){
+    let frame = document.getElementById("navBar").contentDocument;
+    let nav = frame.getElementById("large-navbar");
+    let content = document.getElementsByClassName("content")[0];
+    let curtain = document.getElementsByClassName("curtain")[0];
+    curtain.style.backgroundColor="rgba(0, 0, 0, .8)"
+    curtain.style.display="initial";
+    content.style.transform="translate3d(-57vw, 0, 0)";
+    document.getElementById("navBar").style.transform="translate3d(0, 0, 1px)";
+    document.getElementById("navBar").style.height = "100vh";
+    nav.getElementsByClassName("selected")[0].style.borderBottomWidth = "1vh";
+    frame.getElementById("logo").style.display = "none";
+    frame.getElementById("nav-control").style.marginRight = "60vw"
+    frame.getElementsByTagName("body")[0].appendChild(nav);
+    frame.getElementsByTagName("nav")[0].style.padding = "2vh 1vw";
+    nav.style.display = "flex";
+    nav.style.height = "100vh";
+    nav.style.width = "60vw";
+    nav.style.color = "white";
+}
+
+
 
 /*
 function changeImg(id){
     if (screen.width >= 580) {
-        loadImage(id, "images/quienesNinosTitle.png");
-        document.getElementById(id).src = "images/quienesNinosTitle.png";
+        loadImage(id, "images/violinesPorLaPaz.png");
+        document.getElementById(id).src = "images/violinesPorLaPaz.png";
     } else {
         document.getElementById(id).src = "images/quienesNinosTitleMobile.png";
     }
@@ -70,28 +119,6 @@ function tmp() {
 
  */
 
-
-window.addEventListener("load", loadLinks);
-window.addEventListener("load", markSelected);
-
-function loadLinks(){
-    let frame = document.getElementById("navBar").contentDocument;
-    let links = frame.getElementsByClassName("menuLink");
-    for(let i = 0; i < links.length;i++) {
-        console.log(links[i].id);
-        links[i].onclick = function(){
-            window.sessionStorage["select"] = this.id;
-            window.location = this.dataset.href;
-        }
-    }
-};
-
-function markSelected(){
-    let frame = document.getElementById("navBar").contentDocument;
-    let cont = frame.getElementsByClassName("selected");
-    if (cont.length > 0) cont[0].classList.remove("selected");
-    frame.getElementById(window.sessionStorage.getItem("select")).classList.add("selected");
-};
 
 /*
 frame = document.getElementById("navBar").contentDocument;
